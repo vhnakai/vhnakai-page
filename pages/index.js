d8359bd8376d51259a256c40b2b0581043fda0cf
Vitor Hugo Nakai <hugo.nakai@gmail.com>
2022-01-04 16:16:44 -0300

    update a profile photo

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ const Page = () => {
             maxWidth="100px"
             display="inline-block"
             borderRadius="full"
-            src="/images/umiko.gif"
+            src="/images/sorriso.jpeg"
             alt="Profile image"
           />
         </Box>
